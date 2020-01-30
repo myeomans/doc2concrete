97ec2255393e6f673c6c045ac7bc80e0bbeab563
Mike Yeomans <myeomans@myeomans-mbp.local>
2020-01-29 23:42:03 -0500

    un-parallel

--- R/ngramTokens.R
+++ R/ngramTokens.R
@@ -21,19 +21,20 @@ ngramTokens<-function(texts,
                       overlap=1,
                       sparse=0.99,
                       verbose=FALSE,
-                      vocabmatch=NULL){
+                      vocabmatch=NULL,
+                      num.mc.cores=1){
   if(length(texts)<2) {
     stop("must include multiple texts")
   }
 
   cleanertext<-unlist(parallel::mclapply(texts, cleantext, language, stop.words, punct,
-                                         mc.cores = parallel::detectCores()))
+                                         mc.cores = num.mc.cores))
 
   dgm<-lapply(ngrams, function(x) as.matrix(array(NA, c(length(texts),100))))
   token.list<-list()
   for (ng in 1:length(ngrams)){
     tokens<-unlist(parallel::mclapply(cleanertext, gramstem, wstem=wstem, ngrams=ngrams[ng], language=language,
-                                      mc.cores= parallel::detectCores()))
+                                      mc.cores= num.mc.cores))
     dgm[[ng]] <- as.matrix(quanteda::dfm(tokens))
     dgm[[ng]]<-dgm[[ng]][,colSums(dgm[[ng]])>floor(length(texts)/200)]
     if ((sparse<1)) dgm[[ng]]<-dgm[[ng]][,colMeans(dgm[[ng]]>0)>=(1-sparse)]
